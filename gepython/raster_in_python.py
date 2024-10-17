654f034ad7828b7ed2893d6c93eb91dfd09c872a
lbrodsky <lukas.brodsky@natur.cuni.cz>
2024-10-17 08:32:00 +0200

    raster in python

---
+++ gepython/raster_in_python.py
@@ -0,0 +1,11 @@
+
+
+# Importing numpy
+import numpy as np
+
+# Creating a small numpy array with 3x3 dimensions
+small_array = np.array([[1, 2, 3],
+                        [4, 5, 6],
+                        [7, 8, 9]])
+
+small_array
